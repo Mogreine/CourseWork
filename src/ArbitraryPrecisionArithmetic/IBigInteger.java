1f886db6800832edcf59fbacb5cfd4a61f9623ab
Mogreine <north7311@gmail.com>
2017-10-28 15:19:44 +0400

    bigInt

--- src/ArbitraryPrecisionArithmetic/IBigInteger.java
+++ src/ArbitraryPrecisionArithmetic/IBigInteger.java
@@ -2,7 +2,7 @@ package ArbitraryPrecisionArithmetic;
 
 
 /*
-Я пока думаю, делать его в двоичном представлении или десятичном
+Я пока думаю, делать его в двоичном представлении или десятичном.
 */
 public class IBigInteger {
     byte aByte[];
